4bf9bf8e6f80c3dff379de04e178625e43648cb8
devanshukd04 <devanshukd27@gmail.com>
2022-02-01 18:14:23 +0530

    Second Commit
    Signed-off-by: devanshukd04 <devanshukd27@gmail.com>

---
+++ SDP/main.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, redirect, url_for
+app = Flask(__name__)
+app.config['ENV'] = 'development'
+# creating route
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
